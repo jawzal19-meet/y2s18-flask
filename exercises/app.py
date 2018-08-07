ee81f790bea1ce584382bfd6f5a01e5c43ae4512
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:26:03 +0300

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    pass
+# def home_page():
+#     return ("voleyball")
+
+def favSport():
+    return render_template(
+"index.html")
+
+@app.route('/')
+def backgroundColor():
+    return render_template(
+"index.html")
+
+@app.route('/players')
+def favPlayers():
+	players=["yaman","yanal","hawzan", "lobna"]
+	return render_template(
+		"index.html", 
+		players=players)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
